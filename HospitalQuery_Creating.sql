d458e6abba4e83287b315eeddfe168f3189376e9
celinekaram <74150858+celinekaram@users.noreply.github.com>
2022-07-11 14:39:20 +0300

    deleted old table "Appointments" and added tables "Appointment", "Doctor Schedule"

--- HospitalQuery_Creating.sql
+++ HospitalQuery_Creating.sql
@@ -46,17 +46,6 @@
 --FOREIGN KEY(dep_id) REFERENCES Departements(dep_id),
 --)
 
---CREATE TABLE Appointments(
---emp_id INT,
---patient_id INT,
---room_id INT,
---app_date DATE,
---PRIMARY KEY(emp_id,patient_id),
---FOREIGN KEY(emp_id) REFERENCES Employee(emp_id) ON DELETE CASCADE,
---FOREIGN KEY(patient_id) REFERENCES Patients(patient_id) ON DELETE CASCADE,
---FOREIGN KEY(room_id) REFERENCES Rooms(room_id) ON DELETE SET NULL,
---)
-
 --CREATE TABLE Suppliers(
 --supplier_id INT,
 --supplier_name VARCHAR(50),
@@ -79,6 +68,42 @@
 --	properties VARCHAR(1), --'a' for admin, 'd' for doctor, 'c' for client
 --);
 
---ALTER TABLE Appointments
---ADD app_time TIME;
 
+----7/11/22 additions:
+
+----DELETE OLD TABLE "Appointments" using the graphical interface
+----Medical center's business hours: from 9 to 16
+----DATE - format YYYY-MM-DD ex: 2022-07-09
+----added a check constraint (CHK)  to limit the value range to the duration of an appointment: 30 min
+--constraints (Doctor, date and start time) unique, (room, date, and start time) unique, (patient, date and start time) unique
+
+
+--CREATE TABLE Appointment (
+--    doctor_id INT,
+--	patient_id INT,
+--	room_id INT,
+--    app_date DATE NOT NULL,
+--    StartTime time(0) NOT NULL,
+--    EndTime time(0) NOT NULL,
+--	CONSTRAINT PK_Appointment PRIMARY KEY (doctor_id, app_date, StartTime),
+--	CONSTRAINT CHK_THIRTYMIN CHECK (DATEDIFF(MINUTE, StartTime, EndTime)=30),
+--    CONSTRAINT CHK_StartTime_BusinessHours CHECK (DATEPART(HOUR, StartTime) > = 9 AND DATEPART(HOUR, StartTime) < = 16),
+--    CONSTRAINT CHK_EndTime_BusinessHours CHECK (DATEPART(HOUR, EndTime) > = 9 AND DATEPART(HOUR, DATEADD(SECOND, -1, EndTime)) < = 16),
+--	UNIQUE(room_id, app_date, StartTime),
+--	UNIQUE(patient_id, app_date, StartTime),
+--	FOREIGN KEY(doctor_id) REFERENCES Employee(emp_id) ON DELETE CASCADE,
+--	FOREIGN KEY(patient_id) REFERENCES Patients(patient_id) ON DELETE CASCADE,
+--	FOREIGN KEY(room_id) REFERENCES Rooms(room_id) ON DELETE SET NULL,
+--);
+
+--CREATE TABLE DoctorSchedule(
+--	Date_schedule Date, 
+--	dr_id INT,
+--	availableFrom Time(0),
+--	availableTo Time(0),
+--	PRIMARY KEY(dr_id, availableFrom, Date_schedule),
+--	FOREIGN KEY(dr_id) REFERENCES Employee(emp_id) ON DELETE CASCADE,
+--	CONSTRAINT THIRTYMIN CHECK (DATEDIFF(MINUTE, availableFrom, availableTo)=30),
+--    CONSTRAINT StartTime_BusinessHours CHECK (DATEPART(HOUR, availableFrom) > = 9 AND DATEPART(HOUR, availableFrom) < = 16),
+--    CONSTRAINT EndTime_BusinessHours CHECK (DATEPART(HOUR, availableTo) > = 9 AND DATEPART(HOUR, DATEADD(SECOND, -1, availableTo)) < = 16),
+--);
