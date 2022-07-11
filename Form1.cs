6e4e0ca98818d37eeef1a791be4d9ba441d144f3
Vahe <68563993+xX-VIVO-Xx@users.noreply.github.com>
2022-07-11 20:20:58 +0300

    Add files via upload
    Created windows form to display the medications

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medications
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int rowsCount = populategrid("SELECT * FROM Medications ORDER BY med_name");
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int populategrid(string cmd)
+        {
+            var select = cmd;
+            SqlConnection con=new SqlConnection("
+        }
+
+    }
+}
